b6a4a2116d9391a607d3870cd0550746ede636be
Mohammad-al-rifai <eng.mohammad.syria@gmail.com>
2022-08-06 15:52:48 +0300

    AlRifai Add All his End points

--- lib/shared/network/end_point.dart
+++ lib/shared/network/end_point.dart
@@ -7,5 +7,13 @@ class Urls {
   static const String loginUrl =  baseUrl+"users/login";
   static const String changePasswordUrl =  baseUrl+"users/chgPassword";
   static const String checkAccountUrl =  baseUrl+"users/checkEmail";
+  // Added by Muhammad Al-Rifai.
+  static const String acceptanceRate =  baseUrl+"Donation/acceptance_rate";
+  static const String userPosts =  baseUrl+"Donation/user_posts";
+  static const String getDonors =  baseUrl+"Donation/getDonors/";
+  static const String deleteDonor =  baseUrl+"Donation/delete/";
+  static const String confirmDonor =  baseUrl+"Donation/confirm/";
+
+  // ==*=*=====*======*
 
 }
